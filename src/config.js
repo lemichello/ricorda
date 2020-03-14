ff26c7fea56a040243fea3116c820c29d1e3b301
Lemich Maksym <lemichello.m@gmail.com>
2020-03-14 22:21:21 +0200

    feat(config): changed API url in production mode

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-  apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://ricorda-api-stage.herokuapp.com'
+  apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://ricorda-api.herokuapp.com'
 };
