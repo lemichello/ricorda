37cb05f269a6248d97cd41816b05473633bf7440
Lemich Maksym <lemichello.m@gmail.com>
2020-03-22 20:49:35 +0200

    feat(NoWords): added animation

--- src/components/Ricorda/components/TodayWords/components/NoWords/NoWords.jsx
+++ src/components/Ricorda/components/TodayWords/components/NoWords/NoWords.jsx
@@ -2,6 +2,7 @@ import { Button, NonIdealState } from '@blueprintjs/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NoWords.css';
+import Fade from 'react-reveal/Fade';
 
 export const NoWords = function() {
   const newWordsPageBtn = (
@@ -15,13 +16,15 @@ export const NoWords = function() {
   );
 
   return (
-    <NonIdealState
-      icon={'clean'}
-      title={'No words for today'}
-      description={
-        'There are no words to repeat for today. You can add new ones or come back tomorrow.'
-      }
-      action={newWordsPageBtn}
-    />
+    <Fade timeout={500} top distance={'100px'}>
+      <NonIdealState
+        icon={'clean'}
+        title={'No words for today'}
+        description={
+          'There are no words to repeat for today. You can add new ones or come back tomorrow.'
+        }
+        action={newWordsPageBtn}
+      />
+    </Fade>
   );
 };
