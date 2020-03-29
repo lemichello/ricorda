6408850ec18052b1ff882f9d292834503ecd5b4a
Lemich Maksym <lemichello.m@gmail.com>
2020-03-29 13:42:00 +0300

    refactor(Header): changed text of the sign out modal

--- src/components/Ricorda/components/Header/Header.jsx
+++ src/components/Ricorda/components/Header/Header.jsx
@@ -129,7 +129,7 @@ export const Header = function({ logout, toggleDarkTheme, history }) {
         confirmButtonText={'Yes'}
         icon={'log-out'}
       >
-        <p>Are you sure you want to log out?</p>
+        <p>Are you sure you want to sign out?</p>
       </Alert>
     </div>
   );
