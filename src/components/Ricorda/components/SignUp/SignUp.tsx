2feb5b2d04d66282df17fbdf54189a821c9cea08
Lemich Maksym <lemichello.m@gmail.com>
2020-05-16 14:31:57 +0300

    refactor(SignUp): refactored styles

--- src/components/Ricorda/components/SignUp/SignUp.tsx
+++ src/components/Ricorda/components/SignUp/SignUp.tsx
@@ -1,5 +1,7 @@
+/** @jsx jsx */
+
 import { Button, InputGroup } from '@blueprintjs/core';
-import React, {
+import {
   useEffect,
   useState,
   FunctionComponent,
@@ -10,8 +12,8 @@ import { AuthService } from '../../../../services/authService';
 import { DefaultToaster } from '../../../../helpers/DefaultToaster';
 import { History } from 'history';
 import { Link, Switch, Route, useRouteMatch } from 'react-router-dom';
-import './SignUp.css';
 import VerifyEmail from './components/VerifyEmail/VerifyEmail';
+import { jsx, css, SerializedStyles } from '@emotion/core';
 
 interface IProps {
   history: History;
@@ -67,16 +69,60 @@ const SignUp: FunctionComponent<IProps> = ({ history, userToken }) => {
     history.push('/signup/verify');
   };
 
+  const pageDividerStyles: SerializedStyles = css`
+    display: block;
+    border-top: 1px solid rgba(16, 22, 26, 0.15);
+    width: 80%;
+    margin-top: 15px;
+    margin-bottom: 15px;
+
+    .bp3-dark & {
+      border-top: 1px solid hsla(0, 0%, 100%, 0.15);
+    }
+  `;
+  const pageInputStyles: SerializedStyles = css`
+    margin-bottom: 20px;
+  `;
+
   return (
-    <div className={'page-root'}>
-      <div className={'page-content'} onKeyDown={keyDown}>
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 50px;
+        padding-bottom: 50px;
+        justify-content: center;
+
+        @media (max-width: 576px) {
+          padding-top: 80px;
+        }
+      `}
+    >
+      <div
+        onKeyDown={keyDown}
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          width: 75vw;
+
+          @media (min-width: 576px) {
+            width: 50vw;
+          }
+
+          @media (min-width: 1200px) {
+            width: 25vw;
+          }
+        `}
+      >
         <Switch>
           <Route path={`${path}/verify`} component={VerifyEmail} />
           <Route exact path={path}>
             <h5 className={'bp3-heading'}>Sign up with your email address</h5>
-            <span className={'page-divider'} />
+            <span css={pageDividerStyles} />
             <InputGroup
-              className={'page-input'}
               large
               fill
               type={'email'}
@@ -85,9 +131,9 @@ const SignUp: FunctionComponent<IProps> = ({ history, userToken }) => {
               onChange={(event: ChangeEvent<HTMLInputElement>) =>
                 setEmail(event.target.value)
               }
+              css={pageInputStyles}
             />
             <InputGroup
-              className={'page-input'}
               name={'password'}
               large
               fill
@@ -97,9 +143,9 @@ const SignUp: FunctionComponent<IProps> = ({ history, userToken }) => {
               onChange={(event: ChangeEvent<HTMLInputElement>) =>
                 setPassword(event.target.value)
               }
+              css={pageInputStyles}
             />
             <InputGroup
-              className={'page-input'}
               name={'password-confirm'}
               large
               fill
@@ -109,18 +155,35 @@ const SignUp: FunctionComponent<IProps> = ({ history, userToken }) => {
               onChange={(event: ChangeEvent<HTMLInputElement>) =>
                 setPasswordConfirm(event.target.value)
               }
+              css={pageInputStyles}
             />
             <Button
-              className={'page-btn login-page-sign-up-btn bp3-heading'}
+              className={'bp3-heading'}
               text={'Sign Up'}
               intent={'success'}
               onClick={signUp}
               loading={loading}
               disabled={!isValidCredentials()}
+              css={css`
+                display: block;
+                margin: 10px auto 0 auto;
+                width: 80% !important;
+                text-align: center;
+                border-radius: 15px;
+                height: 35px;
+                text-transform: uppercase;
+              `}
             />
-            <span className={'page-divider'} />
-
-            <p className={'sign-up-page-have-account-text'}>
+            <span css={pageDividerStyles} />
+            <p
+              css={css`
+                & > a:hover {
+                  text-decoration: none;
+                  color: #2b95d6;
+                  transition: color 0.2s;
+                }
+              `}
+            >
               Already have an account? <Link to={'/login'}>Log in</Link>
             </p>
           </Route>
